02a4f85cbf0d470064c155c8abae0dc3ec004f8d
Zakia <chabanezakia@gmail.com>
2019-11-05 12:31:52 +0100

    nous venons de creer une premiere version de notre programme

---
+++ echo.rb
@@ -0,0 +1,15 @@
+while true
+  
+  puts "Bnjour que veux tu faire !"
+  sleep(1)
+  entree_utilisateur = gets.chomp
+  if entree_utilisateur =="Rien" 
+    break
+  elsif entree_utilisateur=="manger"
+    sleep(1)
+    puts "miam miam"
+  elsif entree_utilisateur== "dormir"
+  sleep(1)
+  puts " Bonne sieste"
+  end
+end
